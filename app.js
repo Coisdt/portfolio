ae483165ec80f244c1e19ee447cc6480e0194daa
Francois <cois25@live.com>
2023-04-27 11:31:48 +0200

    Updates

--- app.js
+++ app.js
@@ -3,48 +3,29 @@ const toggleNav = document.querySelector(".toggle-nav-btn");
 const sidebar = document.querySelector(".sidebar");
 const closeBtn = document.querySelector(".sidebar-close");
 
-toggleNav.addEventListener('click', ()=> {
-   sidebar.classList.add("show");
-})
+toggleNav.addEventListener("click", () => {
+  sidebar.classList.add("show");
+});
 
 closeBtn.addEventListener("click", () => {
   sidebar.classList.remove("show");
-  console.log('hi');
+  console.log("hi");
 });
 
+//scroll to
+const contactBtn = document.querySelector(".contact-btn");
+const contactForm = document.querySelector(".contact-container");
+const aboutBtn = document.querySelector(".about-link");
+const aboutSection = document.querySelector(".about-container");
 
+contactBtn.addEventListener("click", () => {
+  contactForm.scrollIntoView({
+    behavior: "smooth",
+  });
+});
+aboutBtn.addEventListener("click", () => {
+  aboutSection.scrollIntoView({
+    behavior: "smooth",
+  });
+});
 
-
-
-
-
-
-
-
-
-
-
-// const navLinks = document.querySelectorAll(".nav-links a");
-
-// console.log(navLinks);
-
-// navLinks.forEach((link) => {
-//   link.addEventListener("click", function () {
-//     // Remove the "active" class from all links
-//     [...navLinks].forEach((link) => link.parentElement.classList.remove("active"));
-
-//     // Add the "active" class to the clicked link
-//     this.parentElement.classList.add("active");
-//   });
-// });
-
-// const navbarLinks = document.querySelectorAll(".nav-links a");
-
-// navbarLinks.forEach((link) => {
-//   link.addEventListener("click", () => {
-//     navbarLinks.forEach((link) => {
-//       link.classList.remove("active");
-//     });
-//     link.classList.add("active");
-//   });
-// });
